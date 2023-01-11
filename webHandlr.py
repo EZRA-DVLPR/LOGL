025f3abf339c54cf8c02179bc5b3acca0042b1cc
Isaiah-Martinez <82429414+Isaiah-Martinez@users.noreply.github.com>
2023-01-11 13:15:45 -0800

    in progress

--- webHandlr.py
+++ webHandlr.py
@@ -1,6 +1,7 @@
 # this file will handle all web connections and functions related to the web
 
 import requests
+import json
 
 url = 'https://howlongtobeat.com/'
 header = {"User-Agent" : "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.1 Safari/605.1.15"}
@@ -10,14 +11,27 @@ cookie = {"cto_bidid" : "3fGYkV9kWkxDSWYwTyUyRmlUVUY1a2dTbTdsaG1ob3o0eHEzcXJNT0l
     "_gid" : "GA1.2.762652137.1671044747",
     "_pbjs_userid_consent_data" : "3524755945110770"}
 
-query = {"q" : "elden"}
+query = {"q" : "pokemon"}
 
 dataa = {"searchType":"games","searchTerms":["elden"],"searchPage":1,"size":20,"searchOptions":{"games":{"userId":0,"platform":"","sortCategory":"popular","rangeCategory":"main","rangeTime":{"min":"null","max":"null"},"gameplay":{"perspective":"","flow":"","genre":""},"rangeYear":{"min":"","max":""},"modifier":""},"users":{"sortCategory":"postcount"},"filter":"","sort":0,"randomizer":0}}
 
-r = requests.get(url, headers=header, data=dataa)    #, cookies=cookie)
+r = requests.get(url, headers=header, params=query)    #, cookies=cookie)
 
 print(r.url)
 print("\n\n")
 
 print(r.status_code)
 print("\n\n")
+
+r = requests.get(url + '?q=ninja', headers=header)
+
+print(r.status_code)
+print("\n\n")
+
+print(r.history)
+
+with open('file.txt', 'w') as f:
+    f.write(r.text)
+
+#if cant get it to work then instead search google for results and pick the one with the 
+# HLTB as the webpage
