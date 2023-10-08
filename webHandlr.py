847a8d46709576ddd2972004e2a694dd48b3cb42
EZRA-DVLPR <derpisontr@gmail.com>
2023-10-08 03:12:30 -0700

    webHandlr comment revisions and better readability

--- webHandlr.py
+++ webHandlr.py
@@ -3,19 +3,16 @@
 from bs4 import BeautifulSoup
 import requests
 
-#initiates the entire web scraping for the entire gameslist
-#returns the web-scraped data in an array
+#initiates web scraping for entire gamelist
+#returns the web-scraped data in an array:
+#   [[GAME NAME, [MAIN STORY, COMPLETIONIST]], [GAME NAME, ['no data']], ...]
 def beginWebScrape(gamelist):
     print('Webscraping has begun!')
     print()
 
-    #data_all will hold the titles and the hours array
-    #hours: main, completionist
-    # OR
-    #hours: 'no data'
     data_all = []
 
-    #iterate through given list
+    #for each game in list, perform search and update data_all with info
     for i in range (len(gamelist)):
         print('Working on game ' + str(i+1) + ' of ' + str(len(gamelist)))
         print()
@@ -23,23 +20,27 @@ def beginWebScrape(gamelist):
         #add a new entry in data_all with the title of the game
         data_all.append([gamelist[i]])
 
-        #fix name of game
-        gamelist[i] = fixGamename(gamelist[i])
+        #generates query based on game name
+        gamelist[i] = queryGenerator(gamelist[i])
         
         #extracts hltbURL
         hltbURL = googleSearch(gamelist[i])
         
-        #checks validity of connection and only proceeds with extraction if google connection is good
+        #if valid google connection was established then appends extracted data
         if (not (hltbURL == 'ERROR ON GOOGLE CONNECTION')):
             #extracts stats on game and appends them to data_all[i]
+
+            #hours: [main, completionist]
+            # OR
+            #hours: ['no data']
             data_all[i].append(hltbExtract(hltbURL))
 
     print('Webscraping process has ended!')
     print()
     return data_all
 
-#gets the url of the hltb for the specified game
-#returns the url as a string
+#gets the url of the hltb query for the specified game
+#returns the hltb url as a string
 def googleSearch(gamename):
 
     #make the google search with the given name
@@ -50,7 +51,7 @@ def googleSearch(gamename):
             raise InterruptedError('No valid connection')
 
     except:
-        #if error with connection then say so
+        #error with connection to Google
         print('ERROR CONNECTING FOR ' + gamename)
         print()
         return 'ERROR ON GOOGLE CONNECTION'
@@ -60,13 +61,13 @@ def googleSearch(gamename):
         soup = BeautifulSoup(googlereq.text, 'lxml')
         match = soup.find('div', class_='egMi0 kCrYT')
 
-        #get the link of the first hit and then return that link
+        #obtain and return first link that matches
         link = match.find('a')
         newlink = link.get('href')
         newurl = newlink[7:newlink.find('&')]
         return newurl
 
-#connects to hltb and gets the gamedata
+#connects to hltb and extracts the gamedata
 #default timeout to 2 seconds
 #returns the extracted data (hours) for the game in an array
 def hltbExtract(url):
@@ -79,13 +80,13 @@ def hltbExtract(url):
             raise InterruptedError('No valid connection')
 
     except:
-        #if error with connection then say so
+        #error connecting to HLTB
         print('ERROR CONNECTING to HLTB ')
         print()
         return 'ERROR ON HLTB CONNECTION'
 
     else:
-        #make a soup and then connect to HLTB website
+        #make a soup and connect to HLTB website
         soup = BeautifulSoup(HLTBreq.text, 'lxml')
 
         #hours will be the extracted data if it exists
@@ -104,11 +105,11 @@ def hltbExtract(url):
             try:
                 #check for main story
                 if (len(match.find_all('h4')) > 0) and (match.find_all('h4')[0].text == 'Main Story'):
-                    hours.append(float(fixGameHours(match.find_all('h5')[0].text[:match.find_all('h5')[0].text.find(' ')])))
+                    hours.append(float(hourDecConvert(match.find_all('h5')[0].text[:match.find_all('h5')[0].text.find(' ')])))
             
                 #check for completionist
                 if (len(match.find_all('h4')) > 2) and (match.find_all('h4')[2].text == 'Completionist'):
-                    hours.append(float(fixGameHours(match.find_all('h5')[2].text[:match.find_all('h5')[2].text.find(' ')])))
+                    hours.append(float(hourDecConvert(match.find_all('h5')[2].text[:match.find_all('h5')[2].text.find(' ')])))
 
                 #if no such fields exist then there is no relevant data
                 if hours == []:
@@ -121,28 +122,19 @@ def hltbExtract(url):
         
         return hours
 
-#corrects the gamename for special characters
-#returns a string that is properly formatted for google searches
-def fixGamename(gamename):
-    #first replace all non-alphanumeric characters that have different formats
-    gamename = gamename.replace('\'','%27')
-
-    gamename = gamename.replace('&','%26')
-    
-    gamename = gamename.replace(':','%3A')
-
-    gamename = gamename.replace(',','%2C')
-
-    gamename = gamename.replace('+','%2B')
+#generates a query to use in google search based on given game name
+#returns a string that is query-safe with no common special characters
+def queryGenerator(gamename):
+    #first replace all commonly used non-alphanumeric characters that have different formats
+    #into a readable format for query
+    gamename = gamename.replace('\'','%27').replace('&','%26').replace(':','%3A').replace(',','%2C').replace('+','%2B')
 
     #turns any spaces into '+'
     gamename = gamename.replace(' ', '+')
     
     return gamename
 
-#corrects the hours if they have the single character containing '1/2' by replacing with '.5' instead
-#returns a properly formatted string
-def fixGameHours(gamehours):
+#returns given string with decimal point instead of fraction
+def hourDecConvert(gamehours):
     gamehours = gamehours.replace('½','.5')
     return gamehours
-
