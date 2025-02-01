b14f849966f58a6522519da949bbc5cf93fab846
EZRA-DVLPR <derpisontr@gmail.com>
2025-02-01 11:47:00 -0800

    updated test to reflect Game struct change

--- tests/scraper_test.go
+++ tests/scraper_test.go
@@ -1,23 +1,40 @@
 package tests
 
 import (
-	"reflect"
 	"testing"
 
 	"github.com/EZRA-DVLPR/GameList/internal/scraper"
 )
 
 func TestScraperFetchHLTB(t *testing.T) {
+	// grab a sample game with some data
 	actual := scraper.FetchHLTB("https://howlongtobeat.com/game/42069")
 
+	// explicitly write what the game data should be
 	expected := scraper.Game{
-		Name:    "Lara Craft Go",
-		Url:     "https://howlongtobeat.com/game/42069",
-		Labels:  []string{"Main Story", "Main + Sides", "Completionist", "All Styles"},
-		Lengths: []string{"--", "--", "--", "--"},
+		Name: "Lara Craft Go",
+		Url:  "https://howlongtobeat.com/game/42069",
+		TimeData: map[string]string{
+			"Completionist": "--",
+			"Main + Sides":  "--",
+			"Main Story":    "--",
+		},
 	}
 
-	if !reflect.DeepEqual(actual, expected) {
-		t.Errorf("Expected %+v, got %+v", expected, actual)
+	// compare the values between the retrieved and hard-coded data
+	if actual.Name != expected.Name {
+		t.Errorf("Expected %q, got %q", expected.Name, actual.Name)
+	}
+	if actual.Url != expected.Url {
+		t.Errorf("Expected %q, got %q", expected.Url, actual.Url)
+	}
+	if actual.TimeData["Completionist"] != expected.TimeData["Completionist"] {
+		t.Errorf("Expected %q, got %q", expected.TimeData["Completionist"], actual.TimeData["Completionist"])
+	}
+	if actual.TimeData["Main + Sides"] != expected.TimeData["Main + Sides"] {
+		t.Errorf("Expected %q, got %q", expected.TimeData["Main + Sides"], actual.TimeData["Main + Sides"])
+	}
+	if actual.TimeData["Main Story"] != expected.TimeData["Main Story"] {
+		t.Errorf("Expected %q, got %q", expected.TimeData["Main Story"], actual.TimeData["Main Story"])
 	}
 }
