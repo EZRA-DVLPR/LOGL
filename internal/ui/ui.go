180c7d4d856dd3d910fdc3d3b3564195074dc169
EZRA-DVLPR <derpisontr@gmail.com>
2025-02-08 11:59:47 -0800

    added fyne to project for making GUI. Also made sample GUI

--- internal/ui/ui.go
+++ internal/ui/ui.go
@@ -1,1 +1,14 @@
-dummy data
+package ui
+
+import (
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/widget"
+)
+
+func StartGUI() {
+	a := app.New()
+	w := a.NewWindow("New Window")
+
+	w.SetContent(widget.NewLabel("Content in the window"))
+	w.ShowAndRun()
+}
