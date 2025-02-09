7a864313ad8bc9b3d5a159960b5a861e90868732
EZRA-DVLPR <derpisontr@gmail.com>
2025-02-09 13:26:54 -0800

    changed name and imports for sqldb -> dbhandler

--- cmd/main.go
+++ cmd/main.go
@@ -4,53 +4,53 @@ import (
 	"fmt"
 
 	// "github.com/EZRA-DVLPR/GameList/internal/mkdown"
+	"github.com/EZRA-DVLPR/GameList/internal/dbhandler"
 	"github.com/EZRA-DVLPR/GameList/internal/scraper"
-	"github.com/EZRA-DVLPR/GameList/internal/sqldb"
 	// "github.com/EZRA-DVLPR/GameList/internal/ui"
 )
 
 func main() {
 	fmt.Println("Program Start")
 
-	// sqldb.CreateDB()
+	// dbhandler.CreateDB()
 
 	//
 	// data := scraper.FetchHLTB("https://howlongtobeat.com/game/155106")
-	// sqldb.AddToDB(data)
+	// dbhandler.AddToDB(data)
 	//
 
 	//
 	// data = scraper.FetchHLTB("https://howlongtobeat.com/game/135862")
-	// sqldb.AddToDB(data)
+	// dbhandler.AddToDB(data)
 	//
 
 	//
 	// data = scraper.FetchHLTB("https://howlongtobeat.com/game/80199")
-	// sqldb.AddToDB(data)
+	// dbhandler.AddToDB(data)
 	//
 
 	//
 	// data = scraper.FetchHLTB("https://howlongtobeat.com/game/2127")
-	// sqldb.AddToDB(data)
+	// dbhandler.AddToDB(data)
 	//
 
 	//
 	// data = scraper.FetchHLTB("https://howlongtobeat.com/game/68151")
-	// sqldb.AddToDB(data)
+	// dbhandler.AddToDB(data)
 	//
 
 	//
 	// data = scraper.FetchHLTB("https://howlongtobeat.com/game/116471")
-	// sqldb.AddToDB(data)
+	// dbhandler.AddToDB(data)
 	//
 
 	//
-	// sqldb.DeleteFromDB(data)
+	// dbhandler.DeleteFromDB(data)
 	//
 
 	//
 	// data = scraper.FetchHLTB("https://howlongtobeat.com/game/42069")
-	// sqldb.AddToDB(data)
+	// dbhandler.AddToDB(data)
 	//
 
 	//
@@ -58,23 +58,23 @@ func main() {
 	//
 	// ui.StartGUI()
 
-	// sqldb.Export(1)
-	// sqldb.Export(2)
+	// dbhandler.Export(1)
+	// dbhandler.Export(2)
 
-	// sqldb.ImportSQL()
-	// sqldb.ImportCSV()
+	// dbhandler.ImportSQL()
+	// dbhandler.ImportCSV()
 
-	sqldb.AddFavorite(scraper.FetchHLTB("https://howlongtobeat.com/game/80199"))
+	dbhandler.AddFavorite(scraper.FetchHLTB("https://howlongtobeat.com/game/80199"))
 
-	sqldb.SortDB("name", "ASC")
-	sqldb.SortDB("name", "DESC")
-	sqldb.SortDB("main", "ASC")
-	sqldb.SortDB("main", "DESC")
+	dbhandler.SortDB("name", "ASC")
+	dbhandler.SortDB("name", "DESC")
+	dbhandler.SortDB("main", "ASC")
+	dbhandler.SortDB("main", "DESC")
 
-	sqldb.RemoveFavorite(scraper.FetchHLTB("https://howlongtobeat.com/game/80199"))
+	dbhandler.RemoveFavorite(scraper.FetchHLTB("https://howlongtobeat.com/game/80199"))
 
-	sqldb.SortDB("name", "ASC")
-	sqldb.SortDB("name", "DESC")
-	sqldb.SortDB("main", "ASC")
-	sqldb.SortDB("main", "DESC")
+	dbhandler.SortDB("name", "ASC")
+	dbhandler.SortDB("name", "DESC")
+	dbhandler.SortDB("main", "ASC")
+	dbhandler.SortDB("main", "DESC")
 }
