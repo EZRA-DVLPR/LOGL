b051f671f349ecb28b654afec9752de36abebfde
EZRA-DVLPR <derpisontr@gmail.com>
2025-02-10 23:18:50 -0800

    completed gog game obtaining method

--- internal/integration/integration.go
+++ internal/integration/integration.go
@@ -12,44 +12,44 @@ import (
 )
 
 type GOGPage struct {
-	SortBy                     string        `json:"sortBy"`
-	Page                       int           `json:"page"`
-	TotalProducts              int           `json:"totalProducts"`
-	TotalPages                 int           `json:"totalPages"`
-	ProductsPerPage            int           `json:"productsPerPage"`
-	ContentSystemCompatibility string        `json:"contentSystemCompatibility"`
-	Tags                       []interface{} `json:"tags"`
-	Products                   []GOGProduct  `json:"products"`
-	UpdatedProductsCount       int           `json:"updatedProductsCount"`
-	HiddenUpdatedProductsCount int           `json:"hiddenUpdatedProductsCount"`
-	AppliedFilters             []interface{} `json:"appliedFilters"`
-	HasHiddenProducts          bool          `json:"hasHiddenProducts"`
+	SortBy                     any          `json:"-"`
+	Page                       int          `json:"page"` // used for checking current page
+	TotalProducts              any          `json:"-"`
+	TotalPages                 int          `json:"totalPages"` // check bounds for number of pages
+	ProductsPerPage            any          `json:"-"`
+	ContentSystemCompatibility any          `json:"-"`
+	Tags                       any          `json:"-"`
+	Products                   []GOGProduct `json:"products"` // the products array will have the titles. i.e. the data i want to extract
+	UpdatedProductsCount       any          `json:"-"`
+	HiddenUpdatedProductsCount any          `json:"-"`
+	AppliedFilters             any          `json:"-"`
+	HasHiddenProducts          any          `json:"-"`
 }
 
 type GOGProduct struct {
-	IsGalaxyCompatible   bool          `json:"isGalaxyCompatible"`
-	Tags                 []string      `json:"tags"`
-	ID                   int           `json:"id"`
-	Availability         []interface{} `json:"availability"`
-	Title                string        `json:"title"` // what i want to extract
-	Image                string        `json:"image"`
-	URL                  string        `json:"url"`
-	WorksOn              []interface{} `json:"worksOn"`
-	Category             string        `json:"category"`
-	Rating               int           `json:"rating"`
-	IsComingSoon         bool          `json:"isComingSoon"`
-	IsMovie              bool          `json:"isMovie"`
-	IsGame               bool          `json:"isGame"`
-	Slug                 string        `json:"slug"`
-	Updates              int           `json:"updates"`
-	IsNew                bool          `json:"isNew"`
-	DLCCount             int           `json:"dlcCount"`
-	ReleaseDate          []interface{} `json:"releaseDate"`
-	IsBaseProductMissing bool          `json:"isBaseProductMissing"`
-	IsHidingDisabled     bool          `json:"isHidingDisabled"`
-	IsInDevelopment      bool          `json:"isInDevelopment"`
-	ExtraInfo            []string      `json:"extraInfo"`
-	IsHidden             bool          `json:"isHidden"`
+	IsGalaxyCompatible   any    `json:"-"`
+	Tags                 any    `json:"-"`
+	ID                   any    `json:"-"`
+	Availability         any    `json:"-"`
+	Title                string `json:"title"` // what i want to extract
+	Image                any    `json:"-"`
+	URL                  any    `json:"-"`
+	WorksOn              any    `json:"-"`
+	Category             any    `json:"-"`
+	Rating               any    `json:"-"`
+	IsComingSoon         any    `json:"-"`
+	IsMovie              any    `json:"-"`
+	IsGame               any    `json:"-"`
+	Slug                 any    `json:"-"`
+	Updates              any    `json:"-"`
+	IsNew                any    `json:"-"`
+	DLCCount             any    `json:"-"`
+	ReleaseDate          any    `json:"-"`
+	IsBaseProductMissing any    `json:"-"`
+	IsHidingDisabled     any    `json:"-"`
+	IsInDevelopment      any    `json:"-"`
+	ExtraInfo            any    `json:"-"`
+	IsHidden             any    `json:"-"`
 }
 
 func GetAllGamesGOG(temp string) {
@@ -65,11 +65,12 @@ func GetAllGamesGOG(temp string) {
 	}
 
 	// authentication cookie
+	// INFO: AUTH COOKIE MUST BE FOR CHROME BROWSER
 	err = godotenv.Load()
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
-	// TEST: gog_us is the name of the cookie i have (US based btw)
+	// TEST: gog_us is the name of the cookie i have (US atm)
 	// what about other nations/countries/regions? eg. EU? Oceania? Asia?
 	req.Header.Set("Cookie", fmt.Sprintf("gog_us=%s", os.Getenv("gogcookie")))
 
@@ -91,9 +92,73 @@ func GetAllGamesGOG(temp string) {
 		return
 	}
 
-	fmt.Println(string(body))
-	// TODO: need to get the total number of pages and repeat process for all pages
-	//
+	// grab the data from the page and parse using json
+	var gogpage GOGPage
+	err = json.Unmarshal(body, &gogpage)
+	if err != nil {
+		log.Fatal("Error decoding JSON:", err)
+	}
 
-	// obtain the title from the previously defined JSON structs
+	var gameList []string
+	// for each page in range [1:totalPages] inclusive, want to grab all games from each page
+	for i := 1; i <= gogpage.TotalPages; i++ {
+		gameList = append(gameList, getGOGGames(i)...) // unpack the elts from the search from each page
+	}
+
+	// we now have the entire list of games
+	for _, game := range gameList {
+		fmt.Println(game)
+	}
+}
+
+func getGOGGames(pagenumber int) (gameList []string) {
+	url := fmt.Sprintf("https://embed.gog.com/account/getFilteredProducts?mediaType=1&page=%d", pagenumber)
+
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		fmt.Println("Error creating request:", err)
+		return
+	}
+
+	// authentication cookie
+	// INFO: AUTH COOKIE MUST BE FOR CHROME BROWSER
+	err = godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	// TEST: gog_us is the name of the cookie i have (US atm)
+	// what about other nations/countries/regions? eg. EU? Oceania? Asia?
+	req.Header.Set("Cookie", fmt.Sprintf("gog_us=%s", os.Getenv("gogcookie")))
+
+	req.Header.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36")
+	req.Header.Set("Accept", "application/json, text/javascript, */*; q=0.01")
+	req.Header.Set("Referer", "https://embed.gog.com/")
+	req.Header.Set("X-Requested-With", "XMLHttpRequest")
+
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("Error reading body:", err)
+		return
+	}
+
+	// grab the data from the page and parse using json
+	var gogpage GOGPage
+	err = json.Unmarshal(body, &gogpage)
+	if err != nil {
+		log.Fatal("Error decoding JSON:", err)
+	}
+
+	// from the current page, get the list of game titles
+	for _, gogproduct := range gogpage.Products {
+		gameList = append(gameList, gogproduct.Title)
+	}
+	return
 }
