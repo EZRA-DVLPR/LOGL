ec3ce2a76c0c6116729d9b4d7e4eb1a19c8c4988
EZRA-DVLPR <derpisontr@gmail.com>
2025-02-12 00:39:08 -0800

    split gog and epic into separate files and got them to work just fine

---
+++ internal/integration/epic.go
@@ -0,0 +1,66 @@
+package integration
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"os"
+)
+
+type EPICPage struct {
+	Success any      `json:"-"`
+	Data    EPICData `json:"data"` // where the data exists
+}
+
+type EPICData struct {
+	Applications       []EPICGame `json:"applications"` // applications array holds the games
+	LegacyApplications any        `json:"-"`
+}
+
+type EPICGame struct {
+	CreatedAt       any    `json:"-"`
+	ApplicationName string `json:"applicationName"` // data i want to extract
+	ApplicationID   any    `json:"-"`
+	PrivacyPolicy   any    `json:"-"`
+	Logo            any    `json:"-"`
+}
+
+func GetAllEpicGamesString(input string) {
+	fmt.Println("Getting products from Epic Games string...")
+
+	var epicpage EPICPage
+	err := json.Unmarshal([]byte(input), &epicpage)
+	if err != nil {
+		log.Fatal("Error decoding json", err)
+	}
+
+	for _, app := range epicpage.Data.Applications {
+		fmt.Println(app.ApplicationName)
+	}
+}
+
+func GetAllEpicGamesFile() {
+	fmt.Println("Getting products from Epic Games txt file...")
+
+	file, err := os.Open("epicgames.txt")
+	if err != nil {
+		log.Fatal("Error reading file:", err)
+	}
+	defer file.Close()
+
+	content, err := io.ReadAll(file)
+	if err != nil {
+		log.Fatal("Error reading file", err)
+	}
+
+	var epicpage EPICPage
+	err = json.Unmarshal(content, &epicpage)
+	if err != nil {
+		log.Fatal("Error decoding json", err)
+	}
+
+	for _, app := range epicpage.Data.Applications {
+		fmt.Println(app.ApplicationName)
+	}
+}
--- internal/integration/gog.go
+++ internal/integration/gog.go
@@ -52,24 +52,6 @@ type GOGProduct struct {
 	IsHidden             any    `json:"-"`
 }
 
-type EPICPage struct {
-	Success any      `json:"-"`
-	Data    EPICData `json:"data"` // where the data exists
-}
-
-type EPICData struct {
-	Applications       []EPICGame `json:"applications"` // applications array holds the games
-	LegacyApplications any        `json:"-"`
-}
-
-type EPICGame struct {
-	CreatedAt       any    `json:"-"`
-	ApplicationName string `json:"applicationName"` // data i want to extract
-	ApplicationID   any    `json:"-"`
-	PrivacyPolicy   any    `json:"-"`
-	Logo            any    `json:"-"`
-}
-
 func GetAllGamesGOG(temp string) {
 	fmt.Println("Getting products from GOG...")
 
@@ -181,43 +163,4 @@ func getGOGGames(pagenumber int) (gameList []string) {
 		gameList = append(gameList, gogproduct.Title)
 	}
 	return
-}
-
-func GetAllEpicGamesString(input string) {
-	fmt.Println("Getting products from Epic Games string...")
-
-	var epicpage EPICPage
-	err := json.Unmarshal([]byte(input), &epicpage)
-	if err != nil {
-		log.Fatal("Error decoding json", err)
-	}
-
-	for _, app := range epicpage.Data.Applications {
-		fmt.Println(app.ApplicationName)
-	}
-}
-
-func GetAllEpicGamesFile() {
-	fmt.Println("Getting products from Epic Games txt file...")
-
-	file, err := os.Open("epicgames.txt")
-	if err != nil {
-		log.Fatal("Error reading file:", err)
-	}
-	defer file.Close()
-
-	content, err := io.ReadAll(file)
-	if err != nil {
-		log.Fatal("Error reading file", err)
-	}
-
-	var epicpage EPICPage
-	err = json.Unmarshal(content, &epicpage)
-	if err != nil {
-		log.Fatal("Error decoding json", err)
-	}
-
-	for _, app := range epicpage.Data.Applications {
-		fmt.Println(app.ApplicationName)
-	}
 }
